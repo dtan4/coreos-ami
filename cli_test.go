ddaf77532acdd49b26316a491a432f4b0ee043e4
Daisuke Fujita <dtanshi45@gmail.com>
2015-12-26 18:45:42 +0900

    Initial commit
    gcli new -F flag -flag=c:String -flag=r:String -flag=t:String -o dtan4 coreos-ami

---
+++ cli_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestRun_versionFlag(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	args := strings.Split("./coreos-ami -version", " ")
+
+	status := cli.Run(args)
+	if status != ExitCodeOK {
+		t.Errorf("expected %d to eq %d", status, ExitCodeOK)
+	}
+
+	expected := fmt.Sprintf("coreos-ami version %s", Version)
+	if !strings.Contains(errStream.String(), expected) {
+		t.Errorf("expected %q to eq %q", errStream.String(), expected)
+	}
+}
+
+func TestRun_cFlag(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	args := strings.Split("./coreos-ami -c", " ")
+
+	status := cli.Run(args)
+	_ = status
+}
+
+func TestRun_rFlag(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	args := strings.Split("./coreos-ami -r", " ")
+
+	status := cli.Run(args)
+	_ = status
+}
+
+func TestRun_tFlag(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	args := strings.Split("./coreos-ami -t", " ")
+
+	status := cli.Run(args)
+	_ = status
+}
